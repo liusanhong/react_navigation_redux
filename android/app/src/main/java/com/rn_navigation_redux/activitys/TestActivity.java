d1feeea9e0029a9a0975c31cbc4d46d771e6230d
LiuQiang <siiii@qq.com>
2019-05-08 17:44:51 +0800

    useless test : activity to rn page

---
+++ android/app/src/main/java/com/rn_navigation_redux/activitys/TestActivity.java
@@ -0,0 +1,10 @@
+package com.rn_navigation_redux.activitys;
+
+import com.facebook.react.ReactActivity;
+
+public class TestActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "NewRNLayout";
+    }
+}
